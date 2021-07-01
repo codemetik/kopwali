e45f92d6359469cc0507dc9c1f1560b1dfc4908b
codemetik <irfanovaiyalkharish03@gmail.com>
2021-07-01 23:57:30 +0700

    membuat data pinjaman di halaman anggota

--- views/nik/home.php
+++ views/nik/home.php
@@ -7,6 +7,12 @@ FROM tb_anggota X
 INNER JOIN tb_simpanan Y ON y.id_anggota = x.id_anggota
 WHERE x.id_user = '".$_SESSION['id_user']."'");
   $data = mysqli_fetch_array($sql);
+
+  function pinjaman($id_user,$kolm, $koneksi){
+    $sql = mysqli_query($koneksi, "SELECT * FROM tb_anggota X INNER JOIN tb_pinjaman Y ON y.id_anggota = x.id_anggota where x.id_user = '".$id_user."'");
+    $data = mysqli_fetch_array($sql);
+    echo rupiah($data[$kolm]);
+  }
 ?>
 <div class="row">
   <div class="col-sm-12">
@@ -26,7 +32,7 @@ WHERE x.id_user = '".$_SESSION['id_user']."'");
             <div class="card border-dark mb-3" style="max-width: 18rem;">
               <div class="card-body text-info bg-dark">
                 <h5 class="card-title">Total Pinjaman</h5>
-                <b><p class="card-text">RP. 0.00</p></b>
+                <b><h3 class="card-text"><?= pinjaman($_SESSION['id_user'],'jumlah_pinjaman', $koneksi); ?></h3></b>
               </div>
               <div class="card-footer bg-transparent border-success">Detail >></div>
             </div>
@@ -35,7 +41,7 @@ WHERE x.id_user = '".$_SESSION['id_user']."'");
             <div class="card border-dark mb-3" style="max-width: 18rem;">
               <div class="card-body text-info bg-dark">
                 <h5 class="card-title">Total Modal Usaha</h5>
-                <p class="card-text">Rp. 0.00</p>
+                <b><h3 class="card-text">Rp. 0.00</h3></b>
               </div>
               <div class="card-footer bg-transparent border-success">Detail >></div>
             </div>
@@ -44,7 +50,7 @@ WHERE x.id_user = '".$_SESSION['id_user']."'");
             <div class="card border-dark mb-3" style="max-width: 18rem;">
               <div class="card-body text-info bg-dark">
                 <h5 class="card-title">Total SHU Keseluruhan</h5>
-                <p class="card-text">Rp. 0.00</p>
+                <b><p class="card-text">Rp. 0.00</p></b>
               </div>
               <div class="card-footer bg-transparent border-success">Detail >></div>
             </div>
--- views/nik/pinjamanku.php
+++ views/nik/pinjamanku.php
@@ -1,7 +1,40 @@
 <div class="row">
 	<div class="col-sm-12">
-		<div class="alert alert-primary">
-			<h5>Halaman Pinjamanku</h5>
+		<div class="card">
+			<div class="card-header bg-info">
+				<h5 class="card-title">Data Pinjaman</h5>		
+			</div>
+			<div class="card-body bg-info">
+				<div class="table-responsive">
+					<table id="example" class="table display">
+						<thead class="table-info">
+							<tr>
+								<th>TGL Pinjam</th>
+								<th>Tgl Entry</th>
+								<th>Jenis Bunga</th>
+								<th>Besaran Bunga</th>
+								<th>Jumlah Pijaman</th>
+								<th>Tenor</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php 
+							$sql = mysqli_query($koneksi, "SELECT *, x.tgl_entry as entry FROM tb_anggota z inner join tb_pinjaman X on x.id_anggota = z.id_anggota INNER JOIN tb_bunga Y ON y.id_bunga = x.id_bunga where id_user = '".$_SESSION['id_user']."'");
+							while ($data = mysqli_fetch_array($sql)) { ?>
+								<tr>
+									<th><?= $data['tgl_pinjam']; ?></th>
+									<th><?= $data['entry']; ?></th>
+									<th><?= $data['jenis_bunga']; ?></th>
+									<th><?= $data['jumlah_bunga']." %"; ?></th>
+									<th><?= rupiah($data['jumlah_pinjaman']); ?></th>
+									<th><?= $data['tenor']." bulan"; ?></th>
+								</tr>
+							<?php }
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
